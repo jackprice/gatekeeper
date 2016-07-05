272fb4e879842b73d73660f9ff4f3d9d82e1e283
Jack Price <jack@wearenifty.co.uk>
2016-07-05 13:24:08 +0100

    Clean up imports from local replication service

--- src/main/java/io/gatekeeper/node/service/replication/LocalReplicationService.java
+++ src/main/java/io/gatekeeper/node/service/replication/LocalReplicationService.java
@@ -2,15 +2,12 @@ package io.gatekeeper.node.service.replication;
 
 import io.gatekeeper.configuration.Configuration;
 import io.gatekeeper.configuration.data.replication.LocalReplicationConfiguration;
-import io.gatekeeper.model.Endpoint;
 import io.gatekeeper.node.service.ReplicationService;
 import io.gatekeeper.node.service.replication.common.Node;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 public class LocalReplicationService extends ReplicationService<LocalReplicationConfiguration> {
